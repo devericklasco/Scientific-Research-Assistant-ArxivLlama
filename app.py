048ae5724caa5b352318614746761c883f23d066
Erick Busuulwa <busuulwaerick@gmail.com>
2025-07-28 10:39:32 +0300

    Added sqlite3

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sqlite3
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 import streamlit as st
